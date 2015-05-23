aef3a8b22bac39919b45b43a04e5be5238596cce
Juri <jurigag@gmail.com>
2015-05-23 22:21:15 +0200

    showQuest 1

--- warsaw-quest/questFactory.js
+++ warsaw-quest/questFactory.js
@@ -11,7 +11,6 @@ var questFactory = {
             update: function () {
                 Quests.update(this);
             },
-
         };
     }
 };
---
+++ warsaw-quest/showQuestForm.js
@@ -0,0 +1,32 @@
+if (Meteor.isClient) {
+    Template.showQuestForm.helpers({
+        /*counter: function () {
+            return Session.get('counter');
+        }*/
+    });
+
+    var getQuestsByStore = function (storeId,stepNumber) {
+        return Storys.find({
+            storeId: storeId,
+            stepNumber: stepNumber,
+        }).fetch()[0];
+    };
+    
+    Template.body.helpers.quests = getQuestsByStore(Template.body.helpers.currentGame,Template.body.helpers.questFormStep);
+
+    Template.playStoryForm.events({
+        'submit form': function (event) {
+            //Session.set('counter', Session.get('counter') + 1);
+            event.preventDefault();
+            var value = event.target.password.value;
+            var res = getQuestsByStore(Template.body.helpers.currentGame,Template.body.helpers.questFormStep);
+            if (res.length > 0 && res[0].password == value) {
+                res = res[0];
+                Template.body.helpers.questFormStep++;
+                Template.body.helpers.quests = getQuestsByStore(Template.body.helpers.currentGame,Template.body.helpers.questFormStep);
+            }
+
+            return false;
+        }
+    });
+}
